9b4b8d14aee2b45e380bf84708b5326d69ef1acf
Rishik Yadav <rishikcaptain@gmail.com>
2021-06-01 20:39:49 +0530

    add commit

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String[] args) {
         int x=9;int y=18;
-        System.out.println(x+y);
+        System.out.println("This is sum: "+x+y);
     }
 }
